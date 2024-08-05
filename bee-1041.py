f4fee5767246a503c9f1522226607068b433de56
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-08-04 23:01:04 -0300

    Create bee-1041.py

---
+++ bee-1041.py
@@ -0,0 +1,19 @@
+#1041 - Coordenadas de um Ponto
+
+x, y = input().split()
+x, y = float(x), float(y)
+
+if x == 0.0 and y == 0.0:
+    print('Origem')   
+elif x != 0.0 and y == 0.0:
+    print('Eixo X')     
+elif x == 0.0 and y != 0.0:
+    print('Eixo Y')    
+elif x > 0.0 and y > 0.0:
+    print('Q1')
+elif x < 0.0 and y > 0.0:
+    print('Q2')
+elif x < 0.0 and y < 0.0:
+    print('Q3')
+elif x > 0.0 and y < 0.0:
+    print('Q4')
