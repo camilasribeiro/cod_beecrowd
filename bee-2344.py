6cd8e44e81a49326638314845adf4da04eb07415
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-08-04 23:04:41 -0300

    Create bee-2344.py

---
+++ bee-2344.py
@@ -0,0 +1,14 @@
+#2344 - Notas da Prova
+
+nota = int(input())
+
+if nota == 0:
+    print('E')
+elif nota > 0 and nota <= 35:
+    print('D')
+elif nota > 35 and nota <= 60:
+    print('C')
+elif nota > 60 and nota <= 85:
+    print('B')
+elif nota > 85 and nota <= 100:
+    print('A')
