02d7c09f6544eca09611ea9961e972f2cf292ceb
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-08-04 23:06:11 -0300

    Create bee-1096.py

---
+++ bee-1096.py
@@ -0,0 +1,14 @@
+#1096 - Sequencia IJ 2
+
+i = 1
+j = 7
+c = 0
+
+while c <= 4:
+    print(f'I={i} J={j}')
+    print(f'I={i} J={j-1}')
+    print(f'I={i} J={j-2}')   
+    i +=2
+    j = 7
+    c += 1
+    
