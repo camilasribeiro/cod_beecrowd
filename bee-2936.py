c266dcd3787d68d9c69c23cdf5651b7843e47369
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-08-04 23:05:41 -0300

    Create bee-2936.py

---
+++ bee-2936.py
@@ -0,0 +1,16 @@
+#2936 - Quanta Mandioca?
+
+curupira = int(input())
+boitata = int(input())
+boto = int(input())
+mapinguari = int(input())
+iara = int(input())
+
+gramas_curupira = 300
+gramas_boitata = 1500
+gramas_boto = 600
+gramas_mapinguari = 1000
+gramas_iara = 150
+
+soma = 225 + (curupira * gramas_curupira) + (boitata * gramas_boitata) + (boto * gramas_boto) + (mapinguari * gramas_mapinguari) + (iara * gramas_iara)
+print(soma)
