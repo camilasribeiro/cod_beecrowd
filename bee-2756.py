2ad65f753fbaf311409cd3b3b3caab597a5ca69f
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-08-04 23:05:36 -0300

    Create bee-2756.py

---
+++ bee-2756.py
@@ -0,0 +1,14 @@
+#2756 - Saída 10
+
+def saida():
+    print('       A')
+    print('      B B')
+    print('     C   C')
+    print('    D     D')
+    print('   E       E')
+    print('    D     D')
+    print('     C   C')
+    print('      B B')
+    print('       A')
+
+saida()
