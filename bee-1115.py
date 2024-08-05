be49be0c8e247c98f6171af9adffc05d54cbd63c
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-08-04 23:03:46 -0300

    Create bee-1115.py

---
+++ bee-1115.py
@@ -0,0 +1,17 @@
+#1115 - Quadrante
+
+x, y = input().split()
+x, y = int(x), int(y)
+
+while x != 0 and y != 0:
+    if x > 0 and y > 0:
+        print('primeiro')
+    elif x < 0 and y > 0:
+        print('segundo')
+    elif x < 0 and y < 0:
+        print('terceiro')
+    elif x > 0 and y < 0:
+        print('quarto')
+    x, y = input().split()
+    x, y = int(x), int(y)
+    
